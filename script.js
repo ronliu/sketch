c75031a5bc155fd2bfb7a114484933372676ef16
Ron Liu <itsronliu@gmail.com>
2021-03-08 18:42:11 -0800

    Clean up script.js

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
 const canvas = document.querySelector('.canvas');
 
-/* Get viewport width and height */
-const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-
-console.log(vw);
-console.log(vh);
-
-/* Set number of pixels based on size of viewport */
 const pixelSize = 10;
-const canvasWidth = Math.ceil(vw / pixelSize);    
-const canvasHeight = Math.ceil(vh / pixelSize);
 
-for (let i = 0; i < canvasWidth * canvasHeight; i++) {
-    let pixel = document.createElement('div');
-    pixel.classList.add('canvas-pixel');
-    pixel.style.background = 'black';
+function fillCanvas() {
+    /* Get viewport width and height */
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 
-    canvas.appendChild(pixel);
-    canvas.style['grid-template-columns'] = `repeat(${canvasWidth}, 1fr)`;
-    canvas.style['grid-template-rows'] = `repeat(${canvasHeight}, 1fr)`;
+    /* Determine width and height of canvas based on how many "pixels" fit in the viewport */
+    const canvasWidth = Math.ceil(vw / pixelSize);    
+    const canvasHeight = Math.ceil(vh / pixelSize);
+
+    /* Create a grid of divs to represent "pixels" */
+    for (let i = 0; i < canvasWidth * canvasHeight; i++) {
+        let pixel = document.createElement('div');
+        pixel.classList.add('canvas-pixel');
+        pixel.style.background = 'black';
+
+        canvas.appendChild(pixel);
+        canvas.style['grid-template-columns'] = `repeat(${canvasWidth}, 1fr)`;
+        canvas.style['grid-template-rows'] = `repeat(${canvasHeight}, 1fr)`;
+    }
+
+    /* Make pixels change color when moused over */
+    let pixels = document.querySelectorAll('.canvas-pixel');
+    pixels.forEach(pixel => {
+        pixel.addEventListener('mouseover', function(e) {
+            e.currentTarget.style.background = '#FFFFFF';
+        });
+    });
 }
 
-let pixels = document.querySelectorAll('.canvas-pixel');
-pixels.forEach(pixel => {
-    pixel.addEventListener('mouseover', function(e) {
-        e.currentTarget.style.background = '#FFFFFF';
-    });
-});
+fillCanvas();
