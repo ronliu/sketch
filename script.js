fb18310bcbd9e9789056d91497ff34223725d0c7
Ron Liu <itsronliu@gmail.com>
2021-03-08 18:00:48 -0800

    Fill canvas with pixels

---
+++ script.js
@@ -0,0 +1,19 @@
+const canvas = document.querySelector('.canvas');
+
+const canvasWidth = 16;
+const canvasHeight = 16;
+
+function paintPixel() {
+
+}
+
+for (let i = 0; i < canvasWidth * canvasHeight; i++) {
+    let pixel = document.createElement('div');
+    pixel.classList.add('canvas-pixel');
+    pixel.style.background = 'black';
+
+    canvas.appendChild(pixel);
+    canvas.style['grid-template-columns'] = `repeat(${canvasWidth}, 1fr)`;
+    canvas.style['grid-template-rows'] = `repeat(${canvasHeight}, 1fr)`;
+}
+
